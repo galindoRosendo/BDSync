0f9ea6ef08e56e7014bec945f31b27c69b0bc320
Rosendo Galindo <rgalindo@papajohnspizza.mx>
2016-10-04 18:18:12 -0500

    args added to console app

--- syncBD/Program.cs
+++ syncBD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace syncBD
 {
@@ -6,8 +7,31 @@ namespace syncBD
 	{
 		public static void Main (string[] args)
 		{
-            syncronizer synker = new syncronizer();
-            synker.run();
+            foreach (var item in args)
+            {
+                if (item.Equals("--test"))
+                {
+                    Console.WriteLine("testing mode");
+                    
+                    string path = Directory.GetCurrentDirectory() + @"\gnditemPJldo1\gnditem20160930.dbf";
+                    String[] allLines = File.ReadAllLines(path);
+                    for (int i = 0; i < 19; i++)
+                    {
+                        Console.WriteLine(allLines[i]);
+                    }
+                    //foreach (var line in allLines)
+                    //{
+                    //    Console.WriteLine(line);
+                    //}
+                    Console.ReadKey();
+                }
+                else
+                {
+                    syncronizer synker = new syncronizer();
+                    synker.run();
+                }
+            }
+
 		}
 	}
 }
